40ccb87c5585eac706b408fd3e40560cd529641d
Esat IBIS <esat.taha.ibis@gmail.com>
2016-02-04 10:54:25 -0500

    Added some files

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by esattahaibis on 2016-02-04.
+ */
+"use strict";
+/* setup your IIFE (Immediately invoked Function Expression) or Self executing anonymous function */
+(function(){
+
+	console.log("JavaScripts works fine");
+
+})();
